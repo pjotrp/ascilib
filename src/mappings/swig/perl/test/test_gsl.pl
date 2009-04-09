8e8b8a1b31bb50de6577d52d79878fedb62abae5
Pjotr Prins <pjotr.public01@thebird.nl>
2009-04-09 19:11:36 +0200

    Full GSL statistics module for Perl

--- src/mappings/swig/perl/test/test_gsl.pl
+++ src/mappings/swig/perl/test/test_gsl.pl
@@ -1,6 +1,14 @@
 use ascilib::gsl;
 
-my $result = gsl::gsl_stats_mean([3,4,5,7],1,4);
-print "returns $result!\n";
+my $data =   [
+        17, 18, 16, 18, 12,
+        20, 18, 20, 20, 22,
+        20, 10, 8, 12, 16,
+        16, 18, 20, 18, 21
+    ];
 
-exit ($result != 4.75);
+my $result1 = gsl::gsl_stats_kurtosis($data, 1, 20);
+my $result2 = gsl::gsl_stats_mean([3,4,5,7],1,4);
+print "returns $result1 and $result2!\n";
+
+exit ($result2 != 4.75);
